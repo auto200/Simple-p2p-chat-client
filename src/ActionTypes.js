3f42ed57726e61307fb89fca0f751ceb89fcded2
auto200 <michal.warac@gmail.com>
2019-04-23 18:45:14 +0200

    Change naming of action

--- src/ActionTypes.js
+++ src/ActionTypes.js
@@ -3,4 +3,4 @@ export const SET_SOCKET = "SET_SOCKET";
 export const ADD_USERS_INFO = "ADD_USERS_INFO";
 export const DELETE_USERS_INFO = "DELETE_USERS_INFO";
 export const ADD_MESSAGE = "ADD_MESSAGE";
-export const SET_IS_ALONE_IN_THE_CHAT = "SET_IS_ALONE_IN_THE_CHAT";
+export const ALONE_IN_THE_CHAT = "ALONE_IN_THE_CHAT";
